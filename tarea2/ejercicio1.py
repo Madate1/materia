c4f7278b42b8e5d9cc2abd754ef7136280e71c3f
Mateo Morocho <mateodvd@gmail.com>
2022-10-28 21:51:26 -0500

    todo

---
+++ tarea2/ejercicio1.py
@@ -0,0 +1,3 @@
+
+"""Crear una clase Gato que tenga 2 atributos, nombre y sonido. También agregar un método que permita saludar, 
+en este método indicar que clase es a la que pertenece y cual es su sonido."""
---
+++ tarea2/ejercicio2.py
@@ -0,0 +1,3 @@
+
+"""Crear una clase Perro con los mismos métodos y atributos del Gato, la particularidad es que este debe 
+indicar en el método saludo, que es un perro y su sonido."""
